584113fd13f2f9ec4748c4c84bf926c7d8f15349
Gabriel Rolim <gabrielsrolim@gmail.com>
2011-08-17 23:14:05 -0300

    Feita a modelagem do banco de dados da app cliente.

--- gerenciador/cliente/models.py
+++ gerenciador/cliente/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    id_cliente = models.BigIntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=40)
+    endereco = models.CharField(max_length=200)
+
+class Venda(models.Model):
+    id_venda = models.BigIntegerField(primary_key=True)
+    data_compra = models.DateField()
+    valor = models.DecimalField(max_digits=19,decimal_places=2)
+    metragem = models.IntegerField()
+    preco_unidade = models.DecimalField(max_digits=19,decimal_places=2)
+    id_produto = models.ForeignKey('Produto')
+    id_cliente = models.ForeignKey('Cliente')
+    
+class Produto(models.Model):
+    id_produto = models.BigIntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    preco_compra = models.DecimalField(max_digits=19,decimal_places=2)
+    preco_venda = models.DecimalField(max_digits=19,decimal_places=2)
+    
+class Fornecedor(models.Model):
+    id_fornecedor = models.BigInteger(primary_key=True)
+    nome = models.CharField(max_length=50)
+    endereco = models.CharFiel(max_length=200)
+    id_produto_fornecedor = models.ForeignKey('Produto')
+    
+class ComprasFornecedor(models.Model):
+    id_compra_fornecedor = models.BigIntegerField(primary_key=True)
+    data_compra = models.DateField()
+    metros_comprados = models.DecimalField(max_digits=19,decimal_places=2)
+    valor_da_compra = models.DecimalField(max_digits=19,decimal_places=2)
+    id_fornecedor = models.ForeignKey('Fornecedor')
+    
+class Stock(modelsModel):
+    id_stock = models.BigIntegerField(primary_key=True)
+    valor = models.DecimalField(max_digits=19,decimal_places=2)
+    compra = models.BooleanField()
+    data = models.DateField()
+    id_compra_fornecedor = models.ForeignKey('ComprasFornecedor')
+    id_venda = models.ForeignKey('Venda')
